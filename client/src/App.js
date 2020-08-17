b22b1725279d0ffe63a5b09071ae99cd143fa492
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2020-08-16 22:24:46 -0400

    Janneth-Williams

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
-
+import BubblePage from "./components/BubblePage";
+import ColorList from "./components/ColorList";
+import PrivateRoute from "./components/utils/PrivateRoute";
+import Bubbles from './components/Bubbles'
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+
+        <PrivateRoute exact path = '/colors'><BubblePage/></PrivateRoute>
+       
+        <PrivateRoute exact path = '/list'><ColorList/></PrivateRoute>
+       <Route exact path =" /list "> <Bubbles/> </Route>
+
+
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,15 +3,30 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import {axiosAuth} from './utils/axiosAuth';
+
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+  const [ dependency, setDependency ] = useState(false)
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+ 
+  useEffect(() => {
+    axiosAuth()
+    .get('colors')
+    .then(res => {
+        setColorList(res.data)
+        setDependency(false)
+    })
+    .catch(err => {
+        console.log(err.response)
+    })
+}, [dependency])
 
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
+      <ColorList colors={colorList} updateColors={setColorList} setDependency={setDependency} />
       <Bubbles colors={colorList} />
     </>
   );
